6f3b91019130ebf0ee3d955ecd32767c159630c0
jiaominlong <jiaominlong@outlook.com>
2017-12-20 21:43:40 +0800

    去掉所有的 字段属性汉语。

--- app/Http/Controllers/IndexScreenController.php
+++ app/Http/Controllers/IndexScreenController.php
@@ -7,64 +7,105 @@ use Illuminate\Support\Facades\DB;
 
 class IndexScreenController extends Controller
 {
+    const THISYEAR = '2017';
+    const LASTYEAR = '2016';
+    const THISMONTH = '11'; # 定义当前月份
+    const LASTMONTH = '10'; # 上个月
+    const THISTIME = '3'; #定义询指数  可以 为 1 2 3 三个值
+    const CATEGORY1 = '全棉休闲衬衫'; #男装内销价格总指数走势
+    const CATEGORY2 = '全棉长裤';  #男装内销价格总指数走势
+    const CATEGORY3 = '休闲羽绒服'; #男装内销价格总指数走势
+    const MARKETCATEGORY = '\'休闲羽绒服\', \'T恤\', \'夹克\', \'棉服\', \'全棉休闲衬衫\', \'全棉长裤\', \'风衣\', \'西服\''; #市场大类销售占比
+
+
+
     //
     public function neixiaoprice(){
-        // 男装内销价格指数
-        $data = DB::select('SELECT month AS \'月份\', times AS \'旬数\', category AS \'类别\', zengsu AS \'增速\', huanbi AS \'环比\', dingji AS \'定基\', tongbi AS \'同比\' FROM indexnxprice ORDER BY month DESC, times DESC');
+        // 男装内销价格指数 V2
+        $data = DB::select('SELECT `month`, times, category, zengsu, huanbi, dingji, tongbi FROM indexnxprice WHERE month = '.self::THISYEAR.self::THISMONTH.' AND times = '.self::THISTIME);
         return json_encode($data);
     }
 
     public function neixiaopricetotalindex(){
-        // 男装内销价格总指数走势
-        $data = DB::select('SELECT month AS \'月份\', dingji AS \'定基\', tongbi AS \'同比\', huanbi AS \'环比\' FROM indexnxpricetotaltrend ORDER BY month DESC');
+        // 男装内销价格总指数走势 V2
+        $data = DB::select('SELECT month, dingji, tongbi, huanbi FROM indexnxpricetotaltrend ORDER BY month DESC LIMIT 12');
         return json_encode($data);
     }
 
     public function neixiaobigcateindex(){
-        // 男装内销价格总指数走势
-        $data = DB::select('SELECT month AS \'月份\', category AS \'类别\', `index` AS \'指数\' FROM indexnxbigcatetrend');
+        // 男装内销价格总指数走势 V2
+        $data = array();
+        $data1 = DB::select('SELECT month, category, `index` FROM indexnxbigcatetrend WHERE category = \''.self::CATEGORY1.'\' ORDER BY month DESC LIMIT 12');
+        $data2 = DB::select('SELECT month, category, `index` FROM indexnxbigcatetrend WHERE category = \''.self::CATEGORY2.'\' ORDER BY month DESC LIMIT 12');
+        $data3 = DB::select('SELECT month, category, `index` FROM indexnxbigcatetrend WHERE category = \''.self::CATEGORY3.'\' ORDER BY month DESC LIMIT 12');
+        $data = array_merge($data1, $data2, $data3);
         return json_encode($data);
     }
 
     public function manjingqiindex(){
-        // 男装景气指数
-        $data = DB::select('SELECT month AS \'月份\', category AS \'类别\', `index` AS \'指数\' FROM indexjingqiindex');
+        // 男装景气指数 V2
+        $data = DB::select('SELECT month, category, `index` FROM indexjingqiindex WHERE month = '.self::THISYEAR.self::THISMONTH.' LIMIT 5');
         return json_encode($data);
     }
 
     public function manjingqiindexindu(){
-        // 男装行业景气指数
-        $data = DB::select('SELECT `month` AS \'月份\', `index` AS \'指数\' FROM indexjingqiindu');
-        return json_encode($data);
+        // 男装行业景气指数 V2
+        $data = DB::select('SELECT `month`, `index` FROM indexjingqiindu WHERE `month` LIKE \''.self::LASTYEAR.'%\' OR `month` LIKE \''.self::THISYEAR.'%\' ');
+        $newDate = new \stdClass();
+        $newDate->{self::THISYEAR} = array();
+        $newDate->{self::LASTYEAR} = array();
+        foreach ($data as $item) {
+            if (strstr($item->month, self::THISYEAR)){
+                array_push($newDate->{self::THISYEAR}, $item);
+            }else{
+                array_push($newDate->{self::LASTYEAR}, $item);
+            }
+        }
+        return json_encode($newDate);
     }
 
     public function manjingqiindexcate(){
-        // 男装类别景气指数
-        $data = DB::select('SELECT `month` AS \'月份\', `category` AS \'类别\', `index` AS \'指数\' FROM indexjingqicate');
+        // 男装类别景气指数 V2
+        $data = array();
+        $data1 = DB::select('SELECT `month`, `category`, `index` FROM indexjingqicate WHERE category = \'生产\' ORDER BY month DESC LIMIT 12');
+        $data2 = DB::select('SELECT `month`, `category`, `index` FROM indexjingqicate WHERE category = \'市场\' ORDER BY month DESC LIMIT 12');
+        $data = array_merge($data1, $data2);
         return json_encode($data);
     }
 
     public function marketcatesale(){
         // 市场大类销售占比
-        $data = DB::select('SELECT `month` AS \'月份\', `category` AS \'类别\', `index` AS \'销售额\' FROM marketcatesale ORDER BY created_at DESC');
+        $data = new \stdClass();
+        $data1 = DB::select('SELECT `month`, `category`, `index` AS \'sale\' FROM marketcatesale WHERE `category` IN ('.self::MARKETCATEGORY.') AND month = '.self::THISYEAR.self::THISMONTH.' LIMIT 10');
+        $data2 = DB::select('SELECT `month`, `category`, `index` AS \'sale\' FROM marketcatesale WHERE `category` IN ('.self::MARKETCATEGORY.') AND month = '.self::LASTYEAR.self::THISMONTH.' LIMIT 10');
+        $data->{self::THISYEAR} = $data1;
+        $data->{self::LASTYEAR} = $data2;
         return json_encode($data);
     }
 
     public function onlinesale(){
-        // 市场交易额区县
-        $data = DB::select('SELECT `month` AS \'月份\', `sale` AS \'销售额\', `salecount` AS \'销量\' FROM indexonlinesale');
+        // 市场交易额区县V2
+        $data = DB::select('SELECT `month`, `sale`, `salecount` FROM indexonlinesale ORDER BY `month` DESC LIMIT 12');
         return json_encode($data);
     }
 
     public function onlinecate(){
-        // 淘宝天猫各大类销售占比
-        $data = DB::select('SELECT `month` AS \'月份\', `category` AS \'类别\', `index` AS \'销售额\' FROM marketcatesale ORDER BY created_at DESC');
+        // 淘宝天猫各大类销售占比 V2
+        $data = new \stdClass();
+        $data->{self::THISYEAR.self::THISMONTH} = array();
+        $data->{self::LASTYEAR.self::THISMONTH} = array();
+        $cateArr = DB::select('SELECT `category` FROM indexonlinecate WHERE `month` = '.self::THISYEAR.self::THISMONTH.' ORDER BY `category`');
+        $data->{self::THISYEAR.self::THISMONTH} = DB::select('SELECT `month`, `category`, `sale` FROM indexonlinecate WHERE `month` = '.self::THISYEAR.self::THISMONTH.' ORDER BY `category`');
+        foreach ($cateArr as $item){
+            $sql = sprintf('SELECT `month`, `category`, `sale` FROM indexonlinecate WHERE `month` = '.self::LASTYEAR.self::THISMONTH.' AND `category` = \'%s\'', $item->category);
+            array_push($data->{self::LASTYEAR.self::THISMONTH}, DB::select($sql)[0]);
+        }
         return json_encode($data);
     }
 
     public function onlinesaledata(){
-        // 淘宝天猫各销售数据
-        $data = DB::select('SELECT `month` AS \'月份\', `category` AS \'类别\', `sale` AS \'销售额（量）\', `huanbi` AS \'环比\', `tongbi` AS \'同比\' FROM indexsaledata ORDER BY created_at DESC');
+        // 淘宝天猫各销售数据 V2
+        $data = DB::select('SELECT `month`, `category`, `sale`, `huanbi`, `tongbi` FROM indexsaledata WHERE month ='.self::THISYEAR.self::THISMONTH.' ORDER BY sale DESC');
         return json_encode($data);
     }
 }
--- app/Http/Controllers/MiddleScreenController.php
+++ app/Http/Controllers/MiddleScreenController.php
@@ -8,42 +8,36 @@ use Illuminate\Support\Facades\DB;
 class MiddleScreenController extends Controller
 {
     public function total (){
-        // 数据汇总
-        $data = DB::select('SELECT month AS \'月份\', category AS \'类别\', unit AS \'单位\', `data` AS \'数值\' FROM middletotal ORDER BY `month` LIMIT 4');
+        // 数据汇总 V2
+        $data = DB::select('SELECT month, category, unit, `data` FROM middletotal ORDER BY `month` LIMIT 4');
         return json_encode($data);
     }
 
     public function marketcate (){
-        // 市场交易类别占比
-        $data = DB::select('SELECT month AS \'月份\', category AS \'类别\', unit AS \'单位\', `data` AS \'数值\' FROM middlemarketcate ORDER BY `month` LIMIT 4');
+        // 市场交易类别占比 V2
+        $data = DB::select('SELECT month, category, unit, `data` FROM middlemarketcate ORDER BY `month` LIMIT 4');
         return json_encode($data);
     }
 
     public function towndata (){
-        // 乡镇热力图
-        $data = DB::select('SELECT `month`,	SUM(`outputadd`) AS outputadd, SUM(`outputsame`) AS outputsame,	SUM(`taxadd`) AS taxadd, SUM(`taxsame`) AS taxsame,	SUM(`workeradd`) AS workeradd, SUM(`workersame`) AS workersame,	SUM(`poweradd`) AS poweradd, SUM(`powersame`) AS powersame,	CASE WHEN town = \'虞山镇\' THEN \'高新开发区\' WHEN town = \'经济开发区\' THEN \'碧溪新区\' ELSE town END AS aaa FROM middletowndata GROUP BY aaa,`month` ORDER BY	`month` DESC LIMIT 13');
+        // 乡镇热力图  V2
+        $data = DB::select('SELECT `month`, `town`, `outputadd`, outputsame, taxadd, taxsame, workeradd, workersame, poweradd, powersame FROM middletowndata');
         $hotmapdata = array();
         function tongbi($add, $same){
             return ($add - $same) / $same;
         }
         foreach ($data as $item){
             $newitem = new \stdClass();
-                $newitem->月份 = $item->month;
-                if ($item->aaa == '高新开发区'){
-                    $newitem->镇区 = '虞山镇+高新开发区';
-                } elseif ($item->aaa == '碧溪新区'){
-                    $newitem->镇区 = '碧溪新区+经济开发区';
-                } else {
-                    $newitem->镇区 = $item->aaa;
-                }
-                $newitem->产值累计 = $item->outputadd;
-                $newitem->产值同比 = tongbi($item->outputadd, $item->outputsame);
-                $newitem->利税累计 = $item->taxadd;
-                $newitem->利税同比 = tongbi($item->taxadd, $item->taxsame);
-                $newitem->用工累计 = $item->workeradd;
-                $newitem->用工同比 = tongbi($item->workeradd, $item->workersame);
-                $newitem->用电累计 = $item->poweradd;
-                $newitem->用电同比 = tongbi($item->poweradd, $item->powersame);
+                $newitem->month = $item->month;
+                $newitem->town = $item->town;
+                $newitem->outputadd = $item->outputadd;
+                $newitem->outputtongbi = tongbi($item->outputadd, $item->outputsame);
+                $newitem->taxadd = $item->taxadd;
+                $newitem->taxtongbi = tongbi($item->taxadd, $item->taxsame);
+                $newitem->workeradd = $item->workeradd;
+                $newitem->workertongbi = tongbi($item->workeradd, $item->workersame);
+                $newitem->poweradd = $item->poweradd;
+                $newitem->powertongbi = tongbi($item->poweradd, $item->powersame);
                 array_push($hotmapdata, $newitem);
         }
         return json_encode($hotmapdata);
@@ -51,33 +45,33 @@ class MiddleScreenController extends Controller
 
     public function bankdata (){
         // 银行资金流量
-        $data = DB::select('SELECT `month` AS \'月份\', `money` AS \'资金量\' FROM middlebankdata ORDER BY `month` DESC LIMIT 12');
+        $data = DB::select('SELECT `month`, `money` FROM middlebankdata ORDER BY `month` DESC LIMIT 12');
         return json_encode($data);
     }
 
     public function chinamap (){
         // 物流、客流热力图
-        $data = DB::select('SELECT middlelogistics.area AS \'区域\', Sum(middlelogistics.sendweight) AS `物流发货量`, Sum(middlelogistics.arriveweight) AS `物流到达量`, (SELECT SUM(count) from middlecustomer mc where mc.area =middlelogistics.area GROUP BY mc.area ) AS \'客流量\'
+        $data = DB::select('SELECT middlelogistics.area , Sum(middlelogistics.sendweight) AS \'sendweight\', Sum(middlelogistics.arriveweight) AS \'arriveweight\', (SELECT SUM(count) from middlecustomer mc where mc.area =middlelogistics.area GROUP BY mc.area ) AS \'customer\'
 FROM middlelogistics GROUP BY middlelogistics.area
 ');
         return json_encode($data);
     }
 
     public function arrivelogistics (){
-        // 银行资金流量
-        $data = DB::select('SELECT `month` AS \'月份\', city AS \'城市\', arriveweight AS \'发货重量（吨）\' FROM middlelogistics ORDER BY sendweight DESC LIMIT 5');
+        // 物流到货量
+        $data = DB::select('SELECT `month`, city, arriveweight FROM middlelogistics ORDER BY arriveweight DESC LIMIT 5');
         return json_encode($data);
     }
 
     public function sendlogistics (){
         // 物流发货量top5
-        $data = DB::select('SELECT `month` AS \'月份\', city AS \'城市\', sendweight AS \'发货重量（吨）\' FROM middlelogistics ORDER BY sendweight DESC LIMIT 5');
+        $data = DB::select('SELECT `month`, city, sendweight FROM middlelogistics ORDER BY sendweight DESC LIMIT 5');
         return json_encode($data);
     }
 
     public function marketsale (){
         // 市场交易TOP15
-        $data = DB::select('SELECT `month` AS \'月份\', `market` AS \'市场\', `money` AS \'交易额\' FROM middlemarketdata ORDER BY `month` DESC LIMIT 16');
+        $data = DB::select('SELECT `month`, `market`, `money` FROM middlemarketdata ORDER BY `month` DESC LIMIT 16');
         return json_encode($data);
     }
 }

d462f99e303b7406c84cc5b7a4452a8ce426dcb3
jiaominlong <jiaominlong@outlook.com>
2017-12-21 09:07:47 +0800

    调整所有取值 条件

--- app/Http/Controllers/ForeignTradeScreenController.php
+++ app/Http/Controllers/ForeignTradeScreenController.php
@@ -7,49 +7,50 @@ use Illuminate\Support\Facades\DB;
 
 class ForeignTradeScreenController extends Controller
 {
+    const THISMONTH = '201711';
     public function index(){
         return "";
     }
 
     public function totals(){
         // 品类出口分析   单位 （万美元）
-        $data = DB::select('select month, money, count from foreigntotals ORDER BY month DESC LIMIT 12');
+        $data = DB::select('select month, money, count from foreigntotals ORDER BY `month` DESC LIMIT 12');
         return json_encode($data);
     }
 
     public function country(){
         // 抵运国家TOP5   单位 （万美元）
-        $data = DB::select('select country, money, percent from foreigncountrys ORDER BY money DESC LIMIT 5');
+        $data = DB::select('select country, money, percent from foreigncountrys WHERE `month` = '.self::THISMONTH.' ORDER BY money DESC LIMIT 5');
         return json_encode($data);
     }
 
     public function area(){
         // 抵运区域分析   单位 （万美元）
-        $data = DB::select('select area, money, percent from foreignareas ORDER BY money DESC LIMIT 5');
+        $data = DB::select('select area, money, percent from foreignareas WHERE `month` = '.self::THISMONTH.' ORDER BY money DESC LIMIT 5');
         return json_encode($data);
     }
 
     public function route(){
         // 一带一路出口分析   单位 （万美元）
-        $data = DB::select('select route, money, percent from foreignroutes ORDER BY money DESC, id DESC LIMIT 6');
+        $data = DB::select('select route, money, percent from foreignroutes WHERE `month` = '.self::THISMONTH.' ORDER BY money DESC, id DESC LIMIT 6');
         return json_encode($data);
     }
 
     public function category(){
         // 品类出口分析   单位 （万美元）
-        $data = DB::select('select category, money, percent from foreigncategorys ORDER BY money DESC, id DESC LIMIT 7');
+        $data = DB::select('select category, money, percent from foreigncategorys WHERE `month` = '.self::THISMONTH.' ORDER BY money DESC, id DESC LIMIT 7');
         return json_encode($data);
     }
 
     public function csindex(){
         // 男装常熟出口价格指数
-        $data = DB::select('select month, dingji, huanbi, tongbi FROM foreigncsindexs ORDER BY month DESC LIMIT 12');
+        $data = DB::select('select month, dingji, huanbi, tongbi FROM foreigncsindexs ORDER BY `month` DESC LIMIT 12');
         return json_encode($data);
     }
 
     public function qgindex(){
         // 男装全国出口价格指数
-        $data = DB::select('select month, dingji, huanbi, tongbi FROM foreignqgindexs ORDER BY month DESC LIMIT 12');
+        $data = DB::select('select month, dingji, huanbi, tongbi FROM foreignqgindexs ORDER BY `month` DESC LIMIT 12');
         return json_encode($data);
     }
 }
--- app/Http/Controllers/MiddleScreenController.php
+++ app/Http/Controllers/MiddleScreenController.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\DB;
 
 class MiddleScreenController extends Controller
 {
+    const THISMONTH = '201711';
+    const LASTMONTH = '201710';
     public function total (){
         // 数据汇总 V2
         $data = DB::select('SELECT month, category, unit, `data` FROM middletotal ORDER BY `month` LIMIT 4');
@@ -15,13 +17,13 @@ class MiddleScreenController extends Controller
 
     public function marketcate (){
         // 市场交易类别占比 V2
-        $data = DB::select('SELECT month, category, unit, `data` FROM middlemarketcate ORDER BY `month` LIMIT 4');
+        $data = DB::select('SELECT month, category, unit, `data` FROM middlemarketcate WHERE `month` = '.self::THISMONTH.' ORDER BY `data` DESC');
         return json_encode($data);
     }
 
     public function towndata (){
         // 乡镇热力图  V2
-        $data = DB::select('SELECT `month`, `town`, `outputadd`, outputsame, taxadd, taxsame, workeradd, workersame, poweradd, powersame FROM middletowndata');
+        $data = DB::select('SELECT `month`, `town`, `outputadd`, outputsame, taxadd, taxsame, workeradd, workersame, poweradd, powersame FROM middletowndata WHERE MONTH = '.self::LASTMONTH.' ORDER BY `town` DESC');
         $hotmapdata = array();
         function tongbi($add, $same){
             return ($add - $same) / $same;
@@ -44,7 +46,7 @@ class MiddleScreenController extends Controller
     }
 
     public function bankdata (){
-        // 银行资金流量
+        // 银行资金流量 V2
         $data = DB::select('SELECT `month`, `money` FROM middlebankdata ORDER BY `month` DESC LIMIT 12');
         return json_encode($data);
     }
@@ -52,26 +54,26 @@ class MiddleScreenController extends Controller
     public function chinamap (){
         // 物流、客流热力图
         $data = DB::select('SELECT middlelogistics.area , Sum(middlelogistics.sendweight) AS \'sendweight\', Sum(middlelogistics.arriveweight) AS \'arriveweight\', (SELECT SUM(count) from middlecustomer mc where mc.area =middlelogistics.area GROUP BY mc.area ) AS \'customer\'
-FROM middlelogistics GROUP BY middlelogistics.area
+FROM middlelogistics GROUP BY middlelogistics.area ORDER BY middlelogistics.area DESC
 ');
         return json_encode($data);
     }
 
     public function arrivelogistics (){
         // 物流到货量
-        $data = DB::select('SELECT `month`, city, arriveweight FROM middlelogistics ORDER BY arriveweight DESC LIMIT 5');
+        $data = DB::select('SELECT `month`, city, arriveweight FROM middlelogistics WHERE `month` = '.self::LASTMONTH.' ORDER BY arriveweight DESC LIMIT 5');
         return json_encode($data);
     }
 
     public function sendlogistics (){
         // 物流发货量top5
-        $data = DB::select('SELECT `month`, city, sendweight FROM middlelogistics ORDER BY sendweight DESC LIMIT 5');
+        $data = DB::select('SELECT `month`, city, sendweight FROM middlelogistics WHERE `month` = '.self::LASTMONTH.' ORDER BY sendweight DESC LIMIT 5');
         return json_encode($data);
     }
 
     public function marketsale (){
         // 市场交易TOP15
-        $data = DB::select('SELECT `month`, `market`, `money` FROM middlemarketdata ORDER BY `month` DESC LIMIT 16');
+        $data = DB::select('SELECT `month`, `market`, `money` FROM middlemarketdata WHERE `month` = '.self::THISMONTH.' ORDER BY `money` DESC');
         return json_encode($data);
     }
 }
